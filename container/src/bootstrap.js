a06b44456fc44aac68096ce806ba333553ade9ff
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-16 10:54:42 -0600

    consumiendo funciones remotas de products a container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
+
+const htmlElement = document.querySelector("#my-products");
+mount(htmlElement);
 
 console.log('Container friend');
